3b7ea632a9c6599cdce185c84eff5ad160c1bfb0
fodevac <fodevac@gmail.com>
2023-06-09 21:12:05 -0500

    Added basic pause and resume functionality, changed prefix from . to /

--- youtubebot.py
+++ youtubebot.py
@@ -13,9 +13,10 @@ import sys
 import subprocess as sp
 from dotenv import load_dotenv
 
+
 load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
-PREFIX = os.getenv('BOT_PREFIX', '.')
+PREFIX = os.getenv('BOT_PREFIX', '/')
 PRINT_STACK_TRACE = os.getenv('PRINT_STACK_TRACE', '1').lower() in ('true', 't', '1')
 BOT_REPORT_COMMAND_NOT_FOUND = os.getenv('BOT_REPORT_COMMAND_NOT_FOUND', '1').lower() in ('true', 't', '1')
 BOT_REPORT_DL_ERROR = os.getenv('BOT_REPORT_DL_ERROR', '0').lower() in ('true', 't', '1')
@@ -129,6 +130,17 @@ async def play(ctx: commands.Context, *args):
             except discord.ClientException: connection = get_voice_client_from_channel_id(voice_state.channel.id)
             connection.play(discord.FFmpegOpusAudio(path), after=lambda error=None, connection=connection, server_id=server_id:
                                                              after_track(error, connection, server_id))
+
+@bot.command(name="pause")
+async def pause(ctx: commands.Context):
+    voice_client = get_voice_client_from_channel_id(ctx.author.voice.channel.id)
+    voice_client.pause()
+
+@bot.command(name="resume")
+async def pause(ctx: commands.Context):
+    voice_client = get_voice_client_from_channel_id(ctx.author.voice.channel.id)
+    voice_client.resume()
+
 
 def get_voice_client_from_channel_id(channel_id: int):
     for voice_client in bot.voice_clients:
